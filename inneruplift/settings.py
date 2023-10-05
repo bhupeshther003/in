59bf074d82a0601981e1637efe7c886fe1c8784d
bhupeshther003 <bhupeshther003@gmail.com>
2023-10-05 16:17:43 +0530

    update

--- inneruplift/settings.py
+++ inneruplift/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'inneruplift.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': '8BbtzgpVVgi02VrtmNv1',
-#         'HOST': 'containers-us-west-75.railway.app',
-#         'PORT': '6609'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '8BbtzgpVVgi02VrtmNv1',
+        'HOST': 'containers-us-west-75.railway.app',
+        'PORT': '6609'
+    }
+}
 
 
 #  email verification 
